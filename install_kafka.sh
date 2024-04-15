0c8f96b77888f9ab03a244674553dee2fed6c17e
DINESH KUMAR <75654917+dineshfullstack@users.noreply.github.com>
2024-04-15 12:47:59 +0530

    Create install_kafka.sh

---
+++ install_kafka.sh
@@ -0,0 +1,48 @@
+# shell script that combines all the commands from the previous steps. This script should be run on your EC2 #instance:
+
+#To use this script:
+
+#Save it to a file, for example install_kafka.sh.
+
+
+#Make it executable:
+#   bash
+ #  chmod +x install_kafka.sh
+
+
+
+#Run it:
+ #  bash
+ #  ./install_kafka.sh
+   
+#!/bin/bash
+
+# Update and install necessary software
+sudo yum update -y
+#sudo yum install -y java-1.8.0-openjdk git
+
+# Download and extract Apache Kafka
+wget https://downloads.apache.org/kafka/2.8.1/kafka_2.13-2.8.1.tgz
+tar xzf kafka_2.13-2.8.1.tgz
+
+# Start Zookeeper and Kafka server
+nohup ~/kafka_2.13-2.8.1/bin/zookeeper-server-start.sh ~/kafka_2.13-2.8.1/config/zookeeper.properties > ~/zookeeper.log 2>&1 &
+nohup ~/kafka_2.13-2.8.1/bin/kafka-server-start.sh ~/kafka_2.13-2.8.1/config/server.properties > ~/kafka.log 2>&1 &
+
+# Enable Kafka and Zookeeper on startup
+echo "nohup ~/kafka_2.13-2.8.1/bin/zookeeper-server-start.sh ~/kafka_2.13-2.8.1/config/zookeeper.properties > ~/zookeeper.log 2>&1 &" | sudo tee -a /etc/rc.d/rc.local
+echo "nohup ~/kafka_2.13-2.8.1/bin/kafka-server-start.sh ~/kafka_2.13-2.8.1/config/server.properties > ~/kafka.log 2>&1 &" | sudo tee -a /etc/rc.d/rc.local
+sudo chmod +x /etc/rc.d/rc.local
+
+# Install sbt for Kafka Manager
+wget https://github.com/sbt/sbt/releases/download/v1.3.13/sbt-1.3.13.tgz
+tar xzf sbt-1.3.13.tgz
+sudo mv sbt /usr/local
+export PATH=/usr/local/sbt/bin:$PATH
+
+# Download and start Kafka Manager
+git clone https://github.com/yahoo/CMAK.git
+cd CMAK
+sbt clean dist
+unzip target/universal/cmak-*.zip -d ~/ 
+nohup ~/cmak-*/bin/cmak -Dconfig.file=conf/application.conf -Dhttp.port=9000 > ~/cmak.log 2>&1 &
